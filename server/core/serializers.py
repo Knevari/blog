7d06bf730bab00040ac34e906948d65b06cea697
Knevari <mateus7319@gmail.com>
2020-12-13 03:05:56 -0300

    added Meta

--- server/core/serializers.py
+++ server/core/serializers.py
@@ -3,10 +3,12 @@ from rest_framework import serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
-    model = Post
-    fields = '__all__'
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    model = Comment
-    fields = '__all__'
+    class Meta:
+        model = Comment
+        fields = '__all__'
