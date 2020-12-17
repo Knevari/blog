7ca68a201a1fbcd0c8d7a6e83864dabdbf072954
Knevari <mateus7319@gmail.com>
2020-12-17 15:12:06 -0300

    Added comments endpoint

--- server/core/views.py
+++ server/core/views.py
@@ -2,7 +2,9 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
-from rest_framework import filters
+from rest_framework import filters, status
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from core.models import Post, Comment, UserProfile, Tag, Like
 from core.serializers import PostSerializer, CommentSerializer, UserSerializer, TagSerializer, LikeSerializer
@@ -14,13 +16,17 @@ class PostViewSet(ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsOwner, IsAuthenticatedOrReadOnly)
     serializer_class = PostSerializer
-    queryset = Post.objects.all().select_related('owner').prefetch_related('tag').order_by("-created_at")
+    queryset = Post.objects.all().select_related(
+        'owner').prefetch_related('tag').order_by("-created_at")
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'tag__title']
 
     @action(detail=True, methods=['get'])
-    def comments(self):
-        return Comment.objects.all().filter(owner=self.owner)
+    def comments(self, request, pk):
+        comments = Comment.objects.all().filter(post=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class CommentViewSet(ModelViewSet):
     authentication_classes = (TokenAuthentication,)
@@ -39,6 +45,7 @@ class TagViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = TagSerializer
+
 
 class LikeViewSet(ModelViewSet):
     queryset = Like.objects.all()
